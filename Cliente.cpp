e8a7bb1d60565c7da36dacc1a0cec1463afa2798
unknown <jaranar4@miumg.edu.gt>
2025-02-16 11:41:43 -0600

    se crearon la clase persona y cliente

---
+++ Cliente.cpp
@@ -0,0 +1,24 @@
+#include "Persona.cpp"
+#include <iostream>
+using namespace std;
+
+class Cliente : Persona{
+	private :
+		string nit;
+		
+	//metodos (constructores y metodos crud) (debemos crear que nivel de acceso tendrán)
+	public :
+		Cliente (){
+		}
+		Cliente (string nom, string ape, string dir, int tel, string fn, string n) : Persona(nom, ape, dir, tel, fn){
+			nit = n;
+		}
+		void leer(){
+			cout<<"Nit: "<< nit<<endl;
+			cout<<"Nombres: "<<nombres<<endl;
+			cout<<"Apellidos: "<<apellidos<<endl;
+			cout<<"Dirección: "<<direccion<<endl;
+			cout<<"Teléfono: "<<telefono<<endl;
+			cout<<"Fecha de Nacimiento: "<<fecha_nacimiento<<endl;
+		}	 
+};
